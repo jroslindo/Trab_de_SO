d9760355da634e1a8028dbe6c3ee18cc0a3438a4
João Paulo Roslindo <jroslindo@localhost.localdomain>
2018-04-23 11:28:21 -0300

    Android em 1.0

--- Android/app/src/main/java/com/example/jroslindo/gerenciamento_de_memoria/MainActivity.java
+++ Android/app/src/main/java/com/example/jroslindo/gerenciamento_de_memoria/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.jroslindo.gerenciamento_de_memoria;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner_tamanho_memoria);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.tamanho_da_memoria, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+
+        Spinner spinner2 = (Spinner) findViewById(R.id.spinner_tamanho_pagina);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.tamanho_da_pagina, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner2.setAdapter(adapter);
+
     }
 
 
