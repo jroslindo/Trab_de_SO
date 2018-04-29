735689b8faf2c1ca1dfc8f1360b75d432ae7916b
Jroslindo <jroslindotab@gmail.com>
2018-04-29 15:36:21 -0300

    falta contabilidade

---
+++ GerenciadorDeMemoria/src/gerenciadordememoria/GerenciadorDeMemoria.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gerenciadordememoria;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author João
+ */
+public class GerenciadorDeMemoria extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        JanelaInicialController primeiraTela = new JanelaInicialController();
+        GerenciadorJanela.obterInstancia().inicializaPalco(primaryStage, primeiraTela);
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
---
+++ GerenciadorDeMemoria/src/gerenciadordememoria/Processo.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gerenciadordememoria;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Gustavo
+ */
+public class Processo {
+    
+    private int id;
+    private int tempoCriacao;
+    private int tempoMorte;
+    private int tamanho;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the tempoCriacao
+     */
+    public int getTempoCriacao() {
+        return tempoCriacao;
+    }
+
+    /**
+     * @param tempoCriacao the tempoCriacao to set
+     */
+    public void setTempoCriacao(int tempoCriacao) {
+        this.tempoCriacao = tempoCriacao;
+    }
+
+    /**
+     * @return the tempoMorte
+     */
+    public int getTempoMorte() {
+        return tempoMorte;
+    }
+
+    /**
+     * @param tempoMorte the tempoMorte to set
+     */
+    public void setTempoMorte(int tempoMorte) {
+        this.tempoMorte = tempoMorte;
+    }
+
+    /**
+     * @return the tamanho
+     */
+    public int getTamanho() {
+        return tamanho;
+    }
+
+    /**
+     * @param tamanho the tamanho to set
+     */
+    public void setTamanho(int tamanho) {
+        this.tamanho = tamanho;
+    }
+    
+    //Funcao que le o arquivo escolhido pelo usuario
+    static public ArrayList<Processo> obterListaProcessos(String diretorio) {
+
+        ArrayList<Processo> processos = new ArrayList();
+        try {
+            FileReader arq = new FileReader(diretorio);
+            BufferedReader lerArq = new BufferedReader(arq);
+            String linha;
+            int i = 0;
+
+            //Laço para popular o arraylist com os processos
+            while ((linha = lerArq.readLine()) != null) {
+                if (linha.trim().equals("")) {
+                    continue;
+                }
+                Processo proc = new Processo();
+                String array[] = linha.split(",");
+                proc.setId(Integer.parseInt(array[0]));
+                proc.setTempoCriacao(Integer.parseInt(array[1]));
+                proc.setTempoMorte(Integer.parseInt(array[2]));
+                proc.setTamanho(Integer.parseInt(array[3]));
+                processos.add(proc);
+                i++;
+
+            }
+ 
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Processo.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Processo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return processos;
+    }
+    
+
+}
